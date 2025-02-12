b9accfd5b1000b081c134856d4dd3978d2719d7b
bobba.r <bobba.r@fk1089-232949l.fkipl.flipkart.com>
2025-02-12 11:44:49 +0530

    user

---
+++ src/com/flipkart/bean/User.java
@@ -0,0 +1,45 @@
+package com.flipkart.bean;
+
+public class User {
+	
+	
+	private String userId;
+    public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	private String name;
+    private String email;
+    private String password;
+    private String role; // Customer, GymOwner, Admin
+    
+    
+    
+
+}
